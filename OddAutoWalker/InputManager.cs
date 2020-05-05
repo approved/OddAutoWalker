b26953b28a5032b1edb897a1679193916da77425
Nick Kennedy <nickkennedy334@gmail.com>
2020-05-05 00:24:17 -0400

    Initial commit

---
+++ OddAutoWalker/InputManager.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace OddAutoWalker
+{
+    public static class InputManager
+    {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetMessageExtraInfo();
+
+        [Flags]
+        private enum InputType
+        {
+            Mouse = 0,
+            Keyboard = 1,
+            Hardware = 2
+        }
+
+        [Flags]
+        private enum KeyEventF
+        {
+            KeyDown = 0x0000,
+            ExtendedKey = 0x0001,
+            KeyUp = 0x0002,
+            Unicode = 0x0004,
+            Scancode = 0x0008,
+        }
+
+        [Flags]
+        private enum MouseDataF
+        {
+            None = 0,
+            XButton1 = 0x0001,
+            XButton2 = 0x0002
+        }
+
+        [Flags]
+        private enum MouseEventF
+        {
+            None = 0,
+            Absolute = 0x8000,
+            HWheel = 0x1000,
+            Move = 0x0001,
+            MoveNoCoalesce = 0x2000,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            VirtualDesk = 0x4000,
+            Wheel = 0x0800,
+            XDown = 0x0080,
+            XUp = 0x0100
+        }
+
+        private struct Input
+        {
+            public int type;
+            public InputUnion u;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct InputUnion
+        {
+            [FieldOffset(0)] public MouseInput mi;
+            [FieldOffset(0)] public KeyboardInput ki;
+            [FieldOffset(0)] public readonly HardwareInput hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MouseInput
+        {
+            public readonly int dx;
+            public readonly int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public readonly uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KeyboardInput
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public readonly uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct HardwareInput
+        {
+            public readonly uint uMsg;
+            public readonly ushort wParamL;
+            public readonly ushort wParamH;
+        }
+
+        public static class Keyboard
+        {
+            public static void KeyDown(ushort keycode)
+            {
+                Input[] inputs =
+                {
+                            new Input
+                            {
+                                type = (int) InputType.Keyboard,
+                                u = new InputUnion
+                                {
+                                    ki = new KeyboardInput
+                                    {
+                                        wVk = 0,
+                                        wScan = keycode,
+                                        dwFlags = (uint) (KeyEventF.KeyDown | KeyEventF.Scancode),
+                                        dwExtraInfo = GetMessageExtraInfo()
+                                    }
+                                }
+                            }
+                        };
+
+                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+            }
+
+            public static void KeyUp(ushort keycode)
+            {
+                Input[] inputs =
+                {
+                            new Input
+                            {
+                                type = (int) InputType.Keyboard,
+                                u = new InputUnion
+                                {
+                                    ki = new KeyboardInput
+                                    {
+                                        wVk = 0,
+                                        wScan = keycode,
+                                        dwFlags = (uint) (KeyEventF.KeyUp | KeyEventF.Scancode),
+                                        dwExtraInfo = GetMessageExtraInfo()
+                                    }
+                                }
+                            }
+                        };
+
+                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+            }
+
+            public static void KeyPress(ushort keycode)
+            {
+                KeyDown(keycode);
+                KeyUp(keycode);
+            }
+        }
+
+        public static class Mouse
+        {
+            public enum Buttons
+            {
+                Left,
+                Right,
+                Middle,
+                X1,
+                X2
+            }
+
+            public static void MouseDown(Buttons button)
+            {
+
+                MouseEventF flags;
+                MouseDataF data;
+
+                (flags, data) = button switch
+                {
+                    Buttons.Left => (MouseEventF.LeftDown, MouseDataF.None),
+                    Buttons.Right => (MouseEventF.RightDown, MouseDataF.None),
+                    Buttons.Middle => (MouseEventF.MiddleDown, MouseDataF.None),
+                    Buttons.X1 => (MouseEventF.XDown, MouseDataF.XButton1),
+                    Buttons.X2 => (MouseEventF.XDown, MouseDataF.XButton2),
+                    _ => throw new Exception("Unknown button type"),
+                };
+
+                Input[] inputs =
+                {
+                            new Input
+                            {
+                                type = (int) InputType.Mouse,
+                                u = new InputUnion
+                                {
+                                    mi = new MouseInput
+                                    {
+                                        mouseData = (uint)data,
+                                        dwFlags = (uint)flags,
+                                        dwExtraInfo = GetMessageExtraInfo()
+                                    }
+                                }
+                            }
+                        };
+
+                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+            }
+
+            public static void MouseUp(Buttons button)
+            {
+
+                MouseEventF flags;
+                MouseDataF data;
+
+                (flags, data) = button switch
+                {
+                    Buttons.Left => (MouseEventF.LeftUp, MouseDataF.None),
+                    Buttons.Right => (MouseEventF.RightUp, MouseDataF.None),
+                    Buttons.Middle => (MouseEventF.MiddleUp, MouseDataF.None),
+                    Buttons.X1 => (MouseEventF.XUp, MouseDataF.XButton1),
+                    Buttons.X2 => (MouseEventF.XUp, MouseDataF.XButton2),
+                    _ => throw new Exception("Unknown button type"),
+                };
+
+                Input[] inputs =
+                {
+                            new Input
+                            {
+                                type = (int) InputType.Mouse,
+                                u = new InputUnion
+                                {
+                                    mi = new MouseInput
+                                    {
+                                        mouseData = (uint)data,
+                                        dwFlags = (uint)flags,
+                                        dwExtraInfo = GetMessageExtraInfo()
+                                    }
+                                }
+                            }
+                        };
+
+                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+            }
+
+            public static void MouseClick(Buttons button)
+            {
+                MouseDown(button);
+                MouseUp(button);
+            }
+
+            public static void MouseDoubleClick(Buttons button)
+            {
+                MouseClick(button);
+                MouseClick(button);
+            }
+        }
+    }
+}
---
+++ OddAutoWalker/Program.cs
@@ -0,0 +1,222 @@
+﻿using LowLevelInput.Hooks;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace OddAutoWalker
+{
+    public class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        private const string ActivePlayerEndpoint = @"https://127.0.0.1:2999/liveclientdata/activeplayer";
+        private const string PlayerListEndpoint = @"https://127.0.0.1:2999/liveclientdata/playerlist";
+        private const string ChampionStatsEndpoint = @"https://raw.communitydragon.org/latest/game/data/characters/";
+
+        private static bool HasProcess = false;
+        private static bool IsExiting = false;
+        private static bool IsIntializingValues = false;
+
+        private static WebClient Client = new WebClient();
+        private static Process LeagueProcess = null;
+
+        private static string ActivePlayerName = string.Empty;
+        private static string ChampionName = string.Empty;
+        private static string RawChampionName = string.Empty;
+
+        private static double ClientAttackSpeed = 0.625;
+        private static double ChampionAttackCastTime = 0.625;
+        private static double ChampionAttackTotalTime = 0.625;
+        private static double ChampionAttackSpeedRatio = 0.625;
+        private static double ChampionAttackDelayPercent = 0.3;
+        private static double ChampionAttackDelayScaling = 1.0;
+        private static double WindupBuffer = 0.033;
+
+        public static double GetSecondsPerAttack() => 1 / ClientAttackSpeed;
+        public static long GetSecondsPerAttackAsLong() => (long)(GetSecondsPerAttack() * 1000);
+        public static double GetWindupDuration() => (((GetSecondsPerAttack() * ChampionAttackDelayPercent) - ChampionAttackCastTime) * ChampionAttackDelayScaling) + ChampionAttackCastTime;
+        public static long GetWindupDurationAsLong() => (long)(GetWindupDuration() * 1000) + (long)(WindupBuffer * 1000);
+
+        public static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            Console.CursorVisible = false;
+
+            Timer orbWalkTimer = new Timer(33.33);
+            orbWalkTimer.Elapsed += AttackSpeedCacheTimer_Elapsed;
+            orbWalkTimer.Elapsed += OrbWalkTimer_Elapsed;
+            orbWalkTimer.Start();
+
+            CheckLeagueProcess();
+
+            Console.ReadLine();
+        }
+
+        private static void InputManager_OnKeyboardEvent(VirtualKeyCode key, KeyState state)
+        {
+            //TODO: Add keybind checks
+        }
+
+        private static DateTime lastInputTime;
+        private static long lastWindupDuration = 0;
+        private static long lastAttackDuration = 0;
+        private static bool activatedChampionTargeting = false;
+        private static void OrbWalkTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (!HasProcess || IsExiting || GetForegroundWindow() != LeagueProcess.MainWindowHandle)
+            {
+                InputManager.Keyboard.KeyUp((ushort)DirectInputKeys.DIK_X);
+                return;
+            }
+            else if(!activatedChampionTargeting)
+            {
+                activatedChampionTargeting = true;
+                InputManager.Keyboard.KeyDown((ushort)DirectInputKeys.DIK_X);
+            }
+
+            double currentMillis = (e.SignalTime - lastInputTime).TotalMilliseconds;
+            if (currentMillis > GetWindupDurationAsLong())
+            { 
+                if (currentMillis > GetSecondsPerAttackAsLong())
+                {
+                    InputManager.Keyboard.KeyDown((ushort)DirectInputKeys.DIK_A);
+                    InputManager.Mouse.MouseClick(InputManager.Mouse.Buttons.Left);
+                    InputManager.Keyboard.KeyUp((ushort)DirectInputKeys.DIK_A);
+                    lastInputTime = e.SignalTime;
+                    //lastWindupDuration = GetWindupDurationAsLong();
+                    //lastAttackDuration = GetSecondsPerAttackAsLong();
+                }
+                else
+                {
+                    InputManager.Mouse.MouseClick(InputManager.Mouse.Buttons.Right);
+                }
+            }
+        }
+
+        private static void CheckLeagueProcess()
+        {
+            while (LeagueProcess is null || !HasProcess)
+            {
+                LeagueProcess = Process.GetProcessesByName("League of Legends").FirstOrDefault();
+                if (LeagueProcess is null || LeagueProcess.HasExited)
+                {
+                    continue;
+                }
+                HasProcess = true;
+                LeagueProcess.EnableRaisingEvents = true;
+                LeagueProcess.Exited += LeagueProcess_Exited;
+            }
+        }
+
+        private static void LeagueProcess_Exited(object sender, EventArgs e)
+        {
+            HasProcess = false;
+            LeagueProcess = null;
+            //Console.Clear();
+            Console.WriteLine("League Process Exited");
+            CheckLeagueProcess();
+        }
+
+        private static void AttackSpeedCacheTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (HasProcess && !IsExiting && !IsIntializingValues)
+            {
+                JToken activePlayerToken = JToken.Parse(Client.DownloadString(ActivePlayerEndpoint));
+                ActivePlayerName = activePlayerToken["summonerName"].ToString();
+
+                if (string.IsNullOrEmpty(ChampionName))
+                {
+                    IsIntializingValues = true;
+                    JToken playerListToken = JToken.Parse(Client.DownloadString(PlayerListEndpoint));
+                    foreach(JToken token in playerListToken)
+                    {
+                        if(token["summonerName"].ToString().Equals(ActivePlayerName))
+                        {
+                            ChampionName = token["championName"].ToString();
+                            string[] rawNameArray = token["rawChampionName"].ToString().Split('_', StringSplitOptions.RemoveEmptyEntries);
+                            RawChampionName = rawNameArray[rawNameArray.Length - 1];
+                        }
+                    }
+
+                    try
+                    {
+                        GetChampionBaseValues(RawChampionName);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine($"{ex.Message} {ex.StackTrace}");
+                    }
+#if DEBUG
+                    Console.Title = $"({ActivePlayerName}) {ChampionName}";
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine($"Attack Speed Ratio: {ChampionAttackSpeedRatio}");
+                    Console.WriteLine($"Windup Percent: {ChampionAttackDelayPercent}");
+#endif
+                    IsIntializingValues = false;
+                }
+                
+                ClientAttackSpeed = activePlayerToken["championStats"]["attackSpeed"].Value<double>();
+#if DEBUG
+                Console.SetCursorPosition(0, 2);
+                Console.WriteLine($"Current AS: {ClientAttackSpeed.ToString().Substring(0, 8)}");
+                Console.WriteLine($"Seconds Per Attack: {GetSecondsPerAttack().ToString().Substring(0, 8)}");
+                Console.WriteLine($"Windup Duration: {GetWindupDuration().ToString().Substring(0, 8)}s + {WindupBuffer}s delay");
+                Console.WriteLine($"Attack Down Time: {(GetSecondsPerAttack() - GetWindupDuration()).ToString().Substring(0, 8)}s");
+#endif
+            }
+        }
+
+        private static void GetChampionBaseValues(string championName)
+        {
+            string lowerChampionName = championName.ToLower();
+            JToken championBinToken = JToken.Parse(Client.DownloadString($"{ChampionStatsEndpoint}{lowerChampionName}/{lowerChampionName}.bin.json"));
+            JToken championRootStats = championBinToken[$"Characters/{championName}/CharacterRecords/Root"];
+            ChampionAttackSpeedRatio = championRootStats["attackSpeedRatio"].Value<double>(); ;
+
+            JToken championAttackDelayOffsetToken = championRootStats["basicAttack"]["mAttackDelayCastOffsetPercent"];
+            JToken championAttackDelayOffsetSpeedRatioToken = championRootStats["basicAttack"]["mAttackDelayCastOffsetPercentAttackSpeedRatio"];
+
+            if(championAttackDelayOffsetSpeedRatioToken?.Value<double?>() != null)
+            {
+                ChampionAttackDelayScaling = championAttackDelayOffsetSpeedRatioToken.Value<double>();
+            }
+
+            if (championAttackDelayOffsetToken?.Value<double?>() == null)
+            {
+                JToken attackTotalTimeToken = championRootStats["basicAttack"]["mAttackTotalTime"];
+                JToken attackCastTimeToken = championRootStats["basicAttack"]["mAttackCastTime"];
+
+                if (attackTotalTimeToken?.Value<double?>() == null && attackCastTimeToken?.Value<double?>() == null)
+                {
+                    string attackName = championRootStats["basicAttack"]["mAttackName"].ToString();
+                    string attackSpell = $"Characters/{attackName.Split(new[] { "BasicAttack" }, StringSplitOptions.RemoveEmptyEntries)[0]}/Spells/{attackName}";
+                    JToken attackSpellToken = championBinToken[attackSpell];
+                    ChampionAttackDelayPercent += attackSpellToken["mSpell"]["delayCastOffsetPercent"].Value<double>();
+                }
+                else
+                {
+                    ChampionAttackTotalTime = attackTotalTimeToken.Value<double>();
+                    ChampionAttackCastTime = attackCastTimeToken.Value<double>(); ;
+
+                    ChampionAttackDelayPercent = ChampionAttackCastTime / ChampionAttackTotalTime;
+                }
+            }
+            else
+            {
+                ChampionAttackDelayPercent += championAttackDelayOffsetToken.Value<double>(); ;
+            }
+        }
+    }
+
+    public enum AttackCommand
+    {
+        AttackMove,
+        AttackChampOnlyMove,
+        Move
+    }
+}
